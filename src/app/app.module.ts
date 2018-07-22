e9976cc6965096d30e3c0d650fd6b298a2fba5ab
Mudathir <mudathirmahgoub@gmail.com>
2018-07-22 18:34:51 -0500

    Example component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AceEditorModule } from 'ng2-ace-editor';
 
 import { AppComponent } from './app.component';
 import { EditorComponent } from './editor/editor.component';
+import { ExamplesComponent } from './examples/examples.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditorComponent
+    EditorComponent,
+    ExamplesComponent
   ],
   imports: [
     BrowserModule,
